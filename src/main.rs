e4bcf38cee71eb1dc34149efbe0503b47be0167f
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2022-12-06 06:34:55 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,20 @@
-use std::{collections::HashMap, fs, ops::Deref, path::PathBuf};
-
-use aws_config::meta::region::RegionProviderChain;
-use aws_types::region::Region;
-use config::ConfigEntry;
-use futures;
-use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 
 pub mod config;
+pub mod outputs;
 
 #[tokio::main]
 async fn main() -> Result<(), aws_sdk_cloudformation::Error> {
     let config_path = PathBuf::from("./config.yaml");
     let config = config::parse(&config_path).unwrap();
 
-    let handles = config.iter().fold(vec![], |mut acc, config_entry| {
-        let handle = tokio::spawn(generate(config_entry));
-        acc.push(handle);
-        return acc;
-    });
-    futures::future::join_all(handles).await;
-
-    return Ok(());
-}
-
-fn load_config() -> Vec<ConfigEntry> {
-    let contents =
-        fs::read_to_string("config.yaml").expect("Should have been able to read the file");
-
-    let config: Vec<ConfigEntry> =
-        serde_yaml::from_str(&contents).expect("Failed to parse the config file");
-
-    return config;
-}
-
-async fn generate(config: &ConfigEntry) -> Result<(), aws_sdk_cloudformation::Error> {
-    let region = match config.region.as_ref() {
-        // how to make it work?
-        Some(provided_region) => Region::new(provided_region),
-        None => RegionProviderChain::default_provider()
-            .region()
+    for config_entry in config {
+        outputs::Stack::new(config_entry.stack_name.unwrap(), config_entry.region)
             .await
-            .unwrap(),
-    };
-    let stack_name = config.stack_name.as_ref().unwrap();
-
-    let sdk_config = aws_config::from_env().region(region).load().await;
-    let client = aws_sdk_cloudformation::Client::new(&sdk_config);
-
-    let result = client.describe_stacks().stack_name(stack_name).send().await;
-    let result = match result {
-        Ok(data) => data,
-        Err(aws_sdk_cloudformation::types::SdkError::ServiceError { err, .. }) => {
-            println!("Service error: {}", err.message().unwrap());
-            return Ok(());
-        }
-        Err(err) => {
-            println!("Unknown error: {}", err.to_string());
-            return Ok(());
-        }
-    };
-
-    let stacks = result.stacks().unwrap_or_else(|| &[]);
-    if stacks.len() == 0 {
-        println!("Stack: {} not found", stack_name);
-        return Ok(());
+            .generate_outputs()
+            .await
+            .unwrap()
     }
-
-    let outputs = stacks
-        .get(0)
-        .expect("Should not get here")
-        .outputs()
-        .unwrap_or_else(|| &[]);
-
-    if outputs.len() == 0 {
-        println!("Stack: {} does not have any outputs", stack_name);
-        return Ok(());
-    }
-
-    let typings_contents = generate_typings_file(outputs);
-    println!("{}", typings_contents);
-
-    let json_contents = generate_json_file(outputs);
-    println!("{}", json_contents);
 
     return Ok(());
-}
-
-fn generate_typings_file(outputs: &[aws_sdk_cloudformation::model::Output]) -> String {
-    let contents = outputs
-        .into_iter()
-        .fold(String::from(""), |mut acc, output| {
-            let output_key = output.output_key().unwrap();
-            let type_entry = format!("{}: string,\n", output_key);
-            acc.push_str(&type_entry);
-            return acc;
-        });
-
-    return format!(
-        "declare module NodeJs {{ interface ProcessEnv {{ {} }} }}",
-        contents
-    );
-}
-
-fn generate_json_file(outputs: &[aws_sdk_cloudformation::model::Output]) -> String {
-    let init: HashMap<&str, &str> = HashMap::new();
-    let contents = outputs.into_iter().fold(init, |mut acc, output| {
-        let key = output.output_key().unwrap();
-        let value = output.output_value().unwrap();
-        acc.insert(key, value);
-        return acc;
-    });
-
-    return serde_json::to_string(&contents).unwrap();
 }
--- src/outputs.rs
+++ src/outputs.rs
@@ -1,1 +1,107 @@
-mod cfn;
+use std::collections::HashMap;
+
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_cloudformation::Region;
+
+pub struct Stack {
+    pub stack_name: String,
+
+    client: aws_sdk_cloudformation::Client,
+}
+
+impl Stack {
+    pub async fn new(stack_name: String, region: Option<String>) -> Self {
+        let region = match region {
+            Some(provided_region) => Region::new(provided_region),
+            None => RegionProviderChain::default_provider()
+                .region()
+                .await
+                .unwrap(),
+        };
+
+        let region_for_config = region.clone();
+        let sdk_config = aws_config::from_env()
+            .region(region_for_config)
+            .load()
+            .await;
+        let client = aws_sdk_cloudformation::Client::new(&sdk_config);
+
+        return Self { stack_name, client };
+    }
+
+    pub async fn generate_outputs(&self) -> Result<(), aws_sdk_cloudformation::Error> {
+        let result = self
+            .client
+            .describe_stacks()
+            .stack_name(&self.stack_name)
+            .send()
+            .await;
+
+        let result = match result {
+            Ok(data) => data,
+            Err(aws_sdk_cloudformation::types::SdkError::ServiceError { err, .. }) => {
+                println!("Service error: {}", err.message().unwrap());
+                return Ok(());
+            }
+            Err(err) => {
+                println!("Unknown error: {}", err.to_string());
+                return Ok(());
+            }
+        };
+
+        let stacks = result.stacks().unwrap_or_else(|| &[]);
+        if stacks.len() == 0 {
+            println!("Stack: {} not found", &self.stack_name);
+            return Ok(());
+        }
+
+        let outputs = stacks
+            .get(0)
+            .expect("Should not get here")
+            .outputs()
+            .unwrap_or_else(|| &[]);
+
+        if outputs.len() == 0 {
+            println!("Stack: {} does not have any outputs", &self.stack_name);
+            return Ok(());
+        }
+
+        let typings_contents = generate_typings_file(outputs);
+        println!("{}", typings_contents);
+
+        let json_contents = generate_json_file(outputs);
+        println!("{}", json_contents);
+
+        return Ok(());
+    }
+}
+
+fn generate_typings_file(outputs: &[aws_sdk_cloudformation::model::Output]) -> String {
+    let contents = outputs
+        .into_iter()
+        .fold(String::from(""), |mut acc, output| {
+            let output_key = output.output_key().unwrap();
+            let type_entry = format!("{}: string,\n", output_key);
+
+            acc.push_str(&type_entry);
+            return acc;
+        });
+
+    return format!(
+        "declare module NodeJs {{ interface ProcessEnv {{ {} }} }}",
+        contents
+    );
+}
+
+fn generate_json_file(outputs: &[aws_sdk_cloudformation::model::Output]) -> String {
+    let init: HashMap<&str, &str> = HashMap::new();
+    let contents = outputs.into_iter().fold(init, |mut acc, output| {
+        let key = output.output_key().unwrap();
+        let value = output.output_value().unwrap();
+
+        acc.insert(key, value);
+        return acc;
+    });
+
+    return serde_json::to_string(&contents).unwrap();
+}
